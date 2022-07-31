68c8d177e372e1d9e3d07fdc637d702c27221cfd
yuta.saikawa <yuta.saikawa@openhouse-group.com>
2022-07-31 15:06:18 +0900

    不使用タブの折りたたみ、複数ウインドウ使用時の制御

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 // タブグループ化対象タブの設定
 const targetTabConditions = {
-    currentWindow: true,
     pinned: false,
     url: ['http://*/*', 'https://*/*'],
 };
@@ -26,32 +25,42 @@ async function setTabGroup() {
     const tabDict = {};
     // 各タブのタブIDとドメイン名を連想配列に格納
     tabs.forEach(tab => {
+        if (!(tab.windowId in tabDict)) tabDict[tab.windowId] = {};
         hostname = new URL(new URL(tab.url)).hostname;
-        if (hostname in tabDict) tabDict[hostname].push(tab.id);
-        else tabDict[hostname] = [tab.id];
+        hostname in tabDict[tab.windowId] ? tabDict[tab.windowId][hostname].push(tab.id) : tabDict[tab.windowId][hostname] = [tab.id];
     });
     // groupCount の値を元にタブグループのカラーを確定
     let groupCount = 0;
-    // ドメイン名ごとにタブグループ作成
-    for (let hostname in tabDict) {
-        if (tabDict[hostname].length > 1) {
-            // タブグループの作成
-            const groupId = await chrome.tabs.group({
-                tabIds: tabDict[hostname]
-            });
-            // タブグループ名、タブグループカラーを確定
-            await chrome.tabGroups.update(groupId, {
-                title: hostname,
-                color: colorList[groupCount]
-            });
-            groupCount++;
-        }
-        // 同じドメインのタブが存在しない場合タブグループを解除する。
-        else {
-            try {
-                const groupId = await chrome.tabs.ungroup(tabDict[hostname][0]);
-            } catch (e) {
-                console.log(e);
+    for (let windowId in tabDict) {
+        // 現在使用中のタブを取得
+        const currentWindowTabs = await chrome.tabs.query({ active: true, windowId: Number(windowId) });
+        // ドメイン名ごとにタブグループ作成
+        for (let hostname in tabDict[windowId]) {
+            if (tabDict[windowId][hostname].length > 1) {
+                // 現在使用中のタブのホスト名とグループのホスト名が同じ場合タブグループを折りたたまない
+                let collapsedTab = !(hostname == new URL(currentWindowTabs[0].url).hostname);
+                // タブグループの作成
+                const groupId = await chrome.tabs.group({
+                    tabIds: tabDict[windowId][hostname],
+                    createProperties: {
+                        windowId: Number(windowId)
+                    }
+                });
+                // タブグループ名、タブグループカラーを確定
+                await chrome.tabGroups.update(groupId, {
+                    title: hostname,
+                    color: colorList[groupCount],
+                    collapsed: collapsedTab
+                });
+                groupCount++;
+            }
+            // 同じドメインのタブが存在しない場合タブグループを解除する。
+            else {
+                try {
+                    const groupId = await chrome.tabs.ungroup(tabDict[windowId][hostname][0]);
+                } catch (e) {
+                    console.log(e);
+                }
             }
         }
     }
@@ -59,10 +68,20 @@ async function setTabGroup() {
 
 // タブ新規作成、更新処理のリスナー
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.status == 'complete') setTabGroup();
+    if (changeInfo.status == 'complete' && tab.status == 'complete') setTabGroup(tabId);
 });
 
 // タブ削除時のリスナー
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-    setTabGroup()
+    setTabGroup(tabId);
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    setTimeout(function () {
+        setTabGroup(activeInfo.tabId);
+    }, 100);
+});
+
+chrome.tabs.onDetached.addListener(function (tabId, detachInfo) {
+    setTabGroup(tabId);
 });
